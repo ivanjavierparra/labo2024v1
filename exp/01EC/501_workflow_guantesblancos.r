ffd420adfd59b31fece31b97eb8630fcd3ed9b36
IvanJavierParra <ivanparra01@gmail.com>
2024-05-02 16:50:33 -0300

    fix en expcolab 01

--- exp/01EC/501_workflow_guantesblancos.r
+++ exp/01EC/501_workflow_guantesblancos.r
@@ -22,7 +22,7 @@ envg$EXPENV$datasets_dir <- "~/buckets/b1/datasets/"
 envg$EXPENV$arch_sem <- "mis_semillas.txt"
 
 # default
-envg$EXPENV$gcloud$RAM <- 64
+envg$EXPENV$gcloud$RAM <- 256
 envg$EXPENV$gcloud$cCPU <- 8
 
 #------------------------------------------------------------------------------
@@ -110,7 +110,7 @@ DR_drifting_guantesblancos <- function( pmyexp, pinputexps, pserver="local")
   param_local$variables_intrames <- FALSE
   # valores posibles
   #  "ninguno", "rank_simple", "rank_cero_fijo", "deflacion", "estandarizar"
-  param_local$metodo <- "rank_simple"
+  param_local$metodo <- "rank_cero_fijo"
 
   return( exp_correr_script( param_local ) ) # linea fija
 }
@@ -155,8 +155,8 @@ FE_historia_guantesblancos <- function( pmyexp, pinputexps, pserver="local")
   # No me engraso las manos con las variables nuevas agregadas por un RF
   # esta parte demora mucho tiempo en correr, y estoy en modo manos_limpias
   param_local$RandomForest$run <- TRUE
-  param_local$RandomForest$num.trees <- 30
-  param_local$RandomForest$max.depth <- 5
+  param_local$RandomForest$num.trees <- 20
+  param_local$RandomForest$max.depth <- 4
   param_local$RandomForest$min.node.size <- 1000
   param_local$RandomForest$mtry <- 40
 
@@ -276,7 +276,7 @@ HT_tuning_guantesblancos <- function( pmyexp, pinputexps, pserver="local")
 
 
   # una Beyesian de Guantes Blancos, solo hace 15 iteraciones
-  param_local$bo_iteraciones <- 100 # iteraciones de la Optimizacion Bayesiana
+  param_local$bo_iteraciones <- 50 # iteraciones de la Optimizacion Bayesiana
 
   return( exp_correr_script( param_local ) ) # linea fija
 }
@@ -318,18 +318,18 @@ corrida_guantesblancos_202109 <- function( pnombrewf, pvirgen=FALSE )
 {
   if( -1 == exp_wf_init( pnombrewf, pvirgen) ) return(0) # linea fija
 
-  DT_incorporar_dataset_default( "DT0001-e001", "competencia_2024.csv.gz")
-  CA_catastrophe_default( "CA0001-e001", "DT0001-e001" )
+  DT_incorporar_dataset_default( "DT0001-e0001", "competencia_2024.csv.gz")
+  CA_catastrophe_default( "CA0001-e0001", "DT0001-e0001" )
 
-  DR_drifting_guantesblancos( "DR0001-e001", "CA0001-e001" )
-  FE_historia_guantesblancos( "FE0001-e001", "DR0001-e001" )
+  DR_drifting_guantesblancos( "DR0001-e0001", "CA0001-e0001" )
+  FE_historia_guantesblancos( "FE0001-e0001", "DR0001-e0001" )
 
-  TS_strategy_guantesblancos_202109( "TS0001-e001", "FE0001-e001" )
+  TS_strategy_guantesblancos_202109( "TS0001-e0001", "FE0001-e0001" )
 
-  HT_tuning_guantesblancos( "HT0001-e001", "TS0001-e001" )
+  HT_tuning_guantesblancos( "HT0001-e0001", "TS0001-e0001" )
 
   # El ZZ depente de HT y TS
-  ZZ_final_guantesblancos( "ZZ0001-e001", c("HT0001-e001","TS0001-e001") )
+  ZZ_final_guantesblancos( "ZZ0001-e0001", c("HT0001-e0001","TS0001-e0001") )
 
 
   exp_wf_end( pnombrewf, pvirgen ) # linea fija
@@ -346,12 +346,12 @@ corrida_guantesblancos_202107 <- function( pnombrewf, pvirgen=FALSE )
   if( -1 == exp_wf_init( pnombrewf, pvirgen) ) return(0) # linea fija
 
   # Ya tengo corrido FE0001 y parto de alli
-  TS_strategy_guantesblancos_202107( "TS0002-e001", "FE0001-e001" )
+  TS_strategy_guantesblancos_202107( "TS0002-e0001", "FE0001-e0001" )
 
-  HT_tuning_guantesblancos( "HT0002-e001", "TS0002-e001" )
+  HT_tuning_guantesblancos( "HT0002-e0001", "TS0002-e0001" )
 
   # El ZZ depente de HT y TS
-  ZZ_final_guantesblancos( "ZZ0002-e001", c("HT0002-e001", "TS0002-e001") )
+  ZZ_final_guantesblancos( "ZZ0002-e0001", c("HT0002-e0001", "TS0002-e0001") )
 
 
   exp_wf_end( pnombrewf, pvirgen ) # linea fija
@@ -363,12 +363,12 @@ corrida_guantesblancos_202107 <- function( pnombrewf, pvirgen=FALSE )
 
 # Hago primero esta corrida que me genera los experimentos
 # DT0001, CA0001, DR0001, FE0001, TS0001, HT0001 y ZZ0001
-corrida_guantesblancos_202109( "gb01-e001" )
+corrida_guantesblancos_202109( "gb01-e0001" )
 
 
 # Luego partiendo de  FE0001
 # genero TS0002, HT0002 y ZZ0002
 
-# corrida_guantesblancos_202107( "gb02-e001" )
+# corrida_guantesblancos_202107( "gb02-e0001" )
 
  
